2b23d90afa7f6f764b8a62e485e25592194e3878
burtik905 <bartoszsielowski@gmail.com>
2025-01-16 13:22:56 +0100

    update

---
+++ src/IntrukcjeWarunkowe.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class InstrukcjeWarunkowe {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj liczbe: ");
+        System.out.println("Podaj wiek: ");
+        int grade = scanner.nextInt();
+        int age = scanner.nextInt();
+
+        boolean positive = grade > 0;
+        boolean negative = grade < 0;
+        boolean equal = grade == 0;
+
+        if (positive) {
+            System.out.println("Liczba jest dodatnia");
+        } else if (negative) {
+            System.out.println("Liczba jest ujemna");
+
+        } else if (equal) {
+            System.out.println("Liczba jest równa zero");
+
+        }
+
+        switch (grade){
+            case 1:
+                System.out.println("Ocena jest niedostateczna");
+                break;
+            case 2:
+                System.out.println("Ocena jest dopuszczająca");
+                break;
+            case 3:
+                System.out.println("Ocena jest dostateczna");
+                break;
+            case 4:
+                System.out.println("Ocena jest dobra");
+                break;
+            case 5:
+                System.out.println("Ocena jest bardzo dobra");
+                break;
+            case 6:
+                System.out.println("Ocena jest celująca");
+                break;
+        }
+        if (age <= 12){
+            System.out.println("Jesteś dzieckiem");
+        } else if (age <= 17) {
+            System.out.println("Jesteś nastolatkiem");
+        } else if (age <= 64) {
+            System.out.println("Jesteś Dorosły");
+        }
+    }
+}
---
+++ src/OperatorsDemo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class OperatorsDemo {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj liczbe: ");
+        int number = scanner.nextInt();
+
+        boolean positive = number > 0;
+        boolean divisible = number % 2 == 0;
+        int  f = 12;
+        int a = ++f;
+        int b = --f;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        if (divisible) {
+            System.out.println("jest podzielna");
+        } else  {
+            System.out.println("nie jest podzielna");
+
+        }
+
+        if (positive) {
+            System.out.println("jest dodatnia");
+        } else {
+            System.out.println("nie jest dodatnia");
+        }
+
+    }
+}
