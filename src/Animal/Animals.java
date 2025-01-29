e08389d7126705ad708fd9a44aaaf778abd24f51
burtik905 <bartoszsielowski@gmail.com>
2025-01-29 18:30:31 +0100

    update

---
+++ src/Animal/Animals.java
@@ -0,0 +1,13 @@
+package Animal;
+
+public class Animals {
+    protected String name;
+
+    public Animals(String name) {
+        this.name = name;
+    }
+
+    public void makeSound() {
+        System.out.println("Zwierze wydaje dzwięk");
+    }
+}
