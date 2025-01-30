13f4cb3bad8e36f5641ced3ac628df30015d2d74
burtik905 <bartoszsielowski@gmail.com>
2025-01-30 14:38:59 +0100

    update

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,33 +1,46 @@
 class Person {
-private String name;
-private int age;
+    String name;
+    int age;
 
-public Person(String name, int age) {
-    this.name = name;
-    this.age = age;
-}
 
-public Person(String name) {
-    this(name, 18);
-}
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-public String getName() {
-    return name;
-}
 
-public void setName(String name) {
-    this.name = name;
-}
+    Person(String name) {
+        this(name, 18);
+    }
 
-public int getAge() {
-    return age;
-}
 
-public void setAge(int age) {
-    this.age = age;
-}
+    String getName() {
+        return name;
+    }
 
-public void displayInfo() {
-    System.out.println("Name: " + name + ", Age: " + age);
-}
+    void setName(String name) {
+        this.name = name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    void setAge(int age) {
+        this.age = age;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Jan", 25);
+        person1.displayInfo();
+
+
+        Person person2 = new Person("Anna");
+        person2.displayInfo();
+    }
 }
