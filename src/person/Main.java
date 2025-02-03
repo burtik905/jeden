3957c9bff751f029fb282fd6d2efe016f096cc18
burtik905 <bartoszsielowski@gmail.com>
2025-02-03 01:28:40 +0100

    update

---
+++ src/person/Main.java
@@ -0,0 +1,12 @@
+package person;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Jan", 25);
+        person1.displayInfo();
+
+        Person person2 = new Person("Anna");
+        person2.displayInfo();
+    }
+}
